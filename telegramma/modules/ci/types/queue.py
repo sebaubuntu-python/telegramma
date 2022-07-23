afa6e6497aa37da22f116a5a2fb97754b05ab328
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-23 15:45:45 +0200

    telegramma: modules: ci: types: queue: Use tabs indentation
    Change-Id: I8ccd6d815bd207856ee6dbe491c5194821b5e6e0

--- telegramma/modules/ci/types/queue.py
+++ telegramma/modules/ci/types/queue.py
@@ -14,27 +14,27 @@ from telegramma.modules.ci.types.job import BaseJob
 _queue: Queue[BaseJob] = Queue()
 
 async def ci_task(bot: Bot):
-    while True:
-        if bot._stopping:
-            break
+	while True:
+		if bot._stopping:
+			break
 
-        job = await _queue.get()
+		job = await _queue.get()
 
-        try:
-            await job.start()
-        except Exception as e:
-            text = "\n".join([
-                "Error: Unhandled exception in job",
-                f"{format_exception(e)}"
-            ])
-            await log_to_logging_chat(bot.application.bot, text)
+		try:
+			await job.start()
+		except Exception as e:
+			text = "\n".join([
+				"Error: Unhandled exception in job",
+				f"{format_exception(e)}"
+			])
+			await log_to_logging_chat(bot.application.bot, text)
 
-        _queue.task_done()
+		_queue.task_done()
 
 async def put_job(job: BaseJob):
-    return await _queue.put(job)
+	return await _queue.put(job)
 
 async def format_queue_list():
-    return "\n".join([
-        f"On queue: {_queue.qsize()}",
-    ])
+	return "\n".join([
+		f"On queue: {_queue.qsize()}",
+	])
